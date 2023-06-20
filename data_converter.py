48a55e32a9ebec88ffa6294934fd6431b796081f
devinlin89 <devinlin89@gmail.com>
2023-06-20 22:27:28 +0700

    Missed to go through 2 semesters in the code

--- data_converter.py
+++ data_converter.py
@@ -12,22 +12,27 @@ def data_converter(output_file):
     for report_type_name, report_type in data.items():
         for grade_name, grade in report_type.items():
             for semester_name, semester in grade.items():
-                for subject_name, subject_details in semester["subjects"].items():
-                    if subject_details is not None:
-                        # Check if the subject exists in the converted data dictionary
-                        if subject_name not in converted_data:
-                            converted_data[subject_name] = {}
+                 if semester_name != "subjects":
+                    for subject_name, subject_details in semester["subjects"].items():
+                        if subject_details is not None:
+                            # Check if the subject exists in the converted data dictionary
+                            if subject_name not in converted_data:
+                                converted_data[subject_name] = {}
 
-                        # Check if the report type exists for the subject
-                        if report_type_name not in converted_data[subject_name]:
-                            converted_data[subject_name][report_type_name] = {}
+                            # Check if the report type exists for the subject
+                            if report_type_name not in converted_data[subject_name]:
+                                converted_data[subject_name][report_type_name] = {}
 
-                        # Check if the grade exists for the report type
-                        if grade_name not in converted_data[subject_name][report_type_name]:
-                            converted_data[subject_name][report_type_name][grade_name] = {}
+                            # Check if the grade exists for the report type
+                            if grade_name not in converted_data[subject_name][report_type_name]:
+                               converted_data[subject_name][report_type_name][grade_name] = {}
 
-                        # Store the subject details under the grade
-                        converted_data[subject_name][report_type_name][grade_name] = subject_details
+                            # Check if the semester exists for the grade
+                            if semester_name not in converted_data[subject_name][report_type_name][grade_name]:
+                                converted_data[subject_name][report_type_name][grade_name][semester_name] = {}
+
+                            # Store the subject details under the semester
+                            converted_data[subject_name][report_type_name][grade_name][semester_name] = subject_details
 
     # Write the converted data to a new JSON file
     with open(output_file, 'w') as file:
