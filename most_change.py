f0cedfbe67d2e03bb68f9bdf76af82e2276f52f4
devinlin89 <devinlin89@gmail.com>
2023-07-13 17:16:00 +0700

    Changed name of function it calls

--- most_change.py
+++ most_change.py
@@ -1,13 +1,13 @@
-from find_subject import find_subject
+from find_subject import find_subject_change
 
 def best_improving(start, end, score_type, subjects_report):
     # Finds best improving score subject
     start_report = {"type": start[0], "grade": start[1], "semester": start[2]}
     end_report = {"type": end[0], "grade": end[1], "semester": end[2]}
-    return find_subject(start_report, end_report, score_type, subjects_report, lambda a, b: a > b)
+    return find_subject_change(start_report, end_report, score_type, subjects_report, lambda a, b: a > b)
 
 def worst_regressing(start, end, score_type, subjects_report):
     # Finds worst regressing score subject
     start_report = {"type": start[0], "grade": start[1], "semester": start[2]}
     end_report = {"type": end[0], "grade": end[1], "semester": end[2]}
-    return find_subject(start_report, end_report, score_type, subjects_report, lambda a, b: a < b)
+    return find_subject_change(start_report, end_report, score_type, subjects_report, lambda a, b: a < b)
