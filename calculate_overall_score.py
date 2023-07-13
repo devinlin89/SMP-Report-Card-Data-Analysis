c80339cb1346f66da19529b8bdffd20e70f87dec
devinlin89 <devinlin89@gmail.com>
2023-07-13 17:13:46 +0700

    Changed slightly to accomodate new features

--- calculate_overall_score.py
+++ calculate_overall_score.py
@@ -2,13 +2,12 @@ def calculate_overall_score(subject):
     # Calculates overall score of subject by finding average of all scores.
     # Attitude is multiplied by 25
 
-    knowledge_score = subject["Knowledge"]
-    skill_score = subject["Skill"]
-    attitude_score = subject["Attitude"]
+    knowledge_score = subject.get("Knowledge")
+    skill_score = subject.get("Skill")
+    attitude_score = subject.get("Attitude")
 
     if skill_score is None or attitude_score is None:
-        return knowledge_score
+        return None
 
-    overall_score = (knowledge_score + skill_score + (attitude_score * 25)) / 3
-
+    overall_score = (knowledge_score + skill_score + attitude_score * 25) / 3
     return overall_score
